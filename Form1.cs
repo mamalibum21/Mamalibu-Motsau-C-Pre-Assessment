461a8453c2376f9244587352ad3b0aea1189d598
Lenovo-User <Lenovo-User@DESKTOP-66DCH94>
2024-01-26 14:44:12 +0200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mamalibu_Motsau_C__Pre_Assessment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text);
+            textBox1.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex > -1)
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            else
+                MessageBox.Show("Select an item in listbox");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex > -1)
+                listBox1.Items[listBox1.SelectedIndex] = textBox1.Text;
+            else
+                MessageBox.Show("Select an item in listbox");
+            textBox1.Clear();
+        }
+    }
+}
